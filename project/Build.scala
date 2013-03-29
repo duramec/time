24397ef8cd6ac0a9c3781ffcb15cc0d5e440f35e
Aaron Bernard <aaron.bernard@duramec.com>
2013-03-29 18:27:37 -0500

    modified build settings

--- project/Build.scala
+++ project/Build.scala
@@ -4,7 +4,7 @@ import Keys._
 object Settings {
   val jvmVersion        = "1.7"
   val buildOrganization = "com.duramec"
-  val buildVersion      = "0.1.2"
+  val buildVersion      = "0.1.3"
   val buildScalaVersion = Version.Scala
   val warnDeadCode      = false
   val buildSettings = Defaults.defaultSettings ++ Seq(
@@ -19,10 +19,9 @@ object Settings {
   val defaultSettings = buildSettings ++ Seq(
     resolvers ++= Seq(
         typesafe,
-        typesafeSnapshot,
         duramec,
-        duramecSnapshot,
-        mavenLocal),
+        duramecSnapshot
+        ),
     compileOrder in Compile := CompileOrder.JavaThenScala,
     compileOrder in Test := CompileOrder.Mixed,
     scalacOptions in (Compile, doc) ++= Seq(
@@ -42,7 +41,6 @@ object Settings {
       ),
     javacOptions in doc := Seq("-source", jvmVersion),
     parallelExecution in Test := false,
-    publishMavenStyle := true,
     publishTo <<= version { (v: String) =>
       if (v.trim.endsWith("SNAPSHOT")) Some(Resolvers.duramecSnapshot)
       else Some(Resolvers.duramec)
@@ -52,12 +50,9 @@ object Settings {
 }
     
 object Resolvers {
-  val typeSafePrefix   = "http://repo.typesafe.com/typesafe"
   val duramecPrefix    = "repo.duramec.com"
+  val typeSafePrefix   = "http://repo.typesafe.com/typesafe"
   val typesafe         = "typesafe" at (typeSafePrefix + "/releases/")
-  val typesafeSnapshot = "snapshot" at (typeSafePrefix + "/snapshots/")
-  val ivyLocal         = "ivy-local" at "file://"+Path.userHome+"/.ivy2/local"
-  val mavenLocal       = "maven-local" at "file://"+Path.userHome+"/.m2/repository"
   val duramec          = Resolver.sftp("duramec", duramecPrefix, "/releases")
   val duramecSnapshot  = Resolver.sftp("duramec-snapshots", duramecPrefix, "/snapshots")
 }
@@ -78,8 +73,8 @@ object Version {
 object Dependency {
   import Version._
   
-  val jodaTime     = "joda-time"                 % "joda-time"         % "2.1"     % "compile"
-  val scalaTest    = "org.scalatest"             % "scalatest_2.10"    % ScalaTest % "test"
+  val jodaTime     = "joda-time"     % "joda-time"         % "2.2"     % "compile"
+  val scalaTest    = "org.scalatest" % "scalatest_2.10"    % ScalaTest % "test"
 }
 
 object TimeBuild extends Build {
